e14eacf055e61a5e836947b512b3c31d1f22ee74
Hasan Eroglu <hasanheroglu@gmail.com>
2021-01-31 22:05:41 +0300

    Added vec3.h and color.h.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
+#include "vec3.h"
+#include "color.h"
+
 int main() {
     // Image 
 
-    const int image_width = 1000;
-    const int image_height = 1000;
+    const int image_width = 256;
+    const int image_height = 256;
 
     // Render
 
@@ -13,17 +16,12 @@ int main() {
     for (int j = image_height-1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
 
     std::cerr << "\nDone.\n" << std::flush;
+    
+    return 0;
 }
