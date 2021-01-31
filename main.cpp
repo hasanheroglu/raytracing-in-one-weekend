516bd06e7667a0e7ab80af544df9ee3381bbff95
Hasan Eroglu <hasanheroglu@gmail.com>
2021-01-31 22:37:42 +0300

    Added ray.h

--- main.cpp
+++ main.cpp
@@ -2,12 +2,30 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray &r){
+    auto unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1-t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main() {
     // Image 
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 1600;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = vec3(0,0,0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto lower_left_corner = origin - vertical/2 - horizontal/2 - vec3(0, 0, focal_length);
 
     // Render
 
@@ -16,7 +34,10 @@ int main() {
     for (int j = image_height-1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << std::flush;
         for (int i = 0; i < image_width; i++) {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_width - 1);
+            ray r = ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
